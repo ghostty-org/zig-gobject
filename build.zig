68c248b704e678ce99bd52a6b0c2ca7397eb9f05
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-09-20 00:49:06 -0500

    use build.zig to release

--- build.zig
+++ build.zig
@@ -1,7 +1,14 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
+    const minisign_key_ = std.posix.getenv("MINISIGN_KEY");
+    const minisign_password_ = std.posix.getenv("MINISIGN_PASSWORD");
+    const date = b.option([]const u8, "date", "date") orelse "1970-01-01";
+    const run_number = b.option([]const u8, "run-number", "run-rumber") orelse "0";
+    const run_attempt = b.option([]const u8, "run-attempt", "run-attempt") orelse "0";
+    const version = b.fmt("{s}-{s}-{s}", .{ date, run_number, run_attempt });
+
     const gobject_codegen_dep = b.dependency(
         "gobject_codegen",
         .{},
@@ -11,7 +18,7 @@ pub fn build(b: *std.Build) void {
 
     const translate_gir_run = b.addRunArtifact(translate_gir_exe);
 
-    const output = translate_gir_run.addPrefixedOutputDirectoryArg("--output-dir=", "bindings");
+    const bindings = translate_gir_run.addPrefixedOutputDirectoryArg("--output-dir=", "bindings");
 
     translate_gir_run.addPrefixedDirectoryArg("--gir-fixes-dir=", b.path("gir-fixes"));
     {
@@ -33,12 +40,31 @@ pub fn build(b: *std.Build) void {
     translate_gir_run.addPrefixedDirectoryArg("--bindings-dir=", gobject_codegen_dep.path("binding-overrides"));
     translate_gir_run.addPrefixedDirectoryArg("--extensions-dir=", gobject_codegen_dep.path("extensions"));
 
-    if (std.posix.getenv("GIR_PATH")) |gir_path| {
-        var it = std.mem.splitScalar(u8, gir_path, ':');
-        while (it.next()) |path| {
-            translate_gir_run.addPrefixedDirectoryArg("--gir-dir=", std.Build.LazyPath{ .cwd_relative = path });
+    const gir_files = b.addWriteFiles();
+
+    if (std.posix.getenv("GIR_PATH")) |gir_paths| {
+        var gir_path_iterator = std.mem.splitScalar(u8, gir_paths, ':');
+        while (gir_path_iterator.next()) |gir_path| {
+            var gir_dir = try std.fs.cwd().openDir(gir_path, .{ .iterate = true });
+            defer gir_dir.close();
+            var gir_dir_iterator = gir_dir.iterate();
+            while (try gir_dir_iterator.next()) |entry| {
+                const ext = std.fs.path.extension(entry.name);
+
+                if (!std.mem.eql(u8, ext, ".gir")) continue;
+
+                _ = gir_files.addCopyFile(
+                    std.Build.LazyPath{ .cwd_relative = b.pathJoin(&.{ gir_path, entry.name }) },
+                    entry.name,
+                );
+            }
         }
     }
+
+    translate_gir_run.addPrefixedDirectoryArg(
+        "--gir-dir=",
+        gir_files.getDirectory(),
+    );
 
     translate_gir_run.addArg("Adw-1");
     translate_gir_run.addArg("GLib-2.0");
@@ -56,9 +82,152 @@ pub fn build(b: *std.Build) void {
     translate_gir_run.addArg("Xdp-1.0");
     translate_gir_run.addArg("XdpGtk4-1.0");
 
-    b.installDirectory(.{
-        .source_dir = output,
-        .install_dir = .{ .custom = "" },
-        .install_subdir = "",
-    });
+    var artifacts: std.ArrayList(std.Build.LazyPath) = .empty;
+    var files_to_sign: std.ArrayList(struct { name: []const u8, lp: std.Build.LazyPath }) = .empty;
+
+    {
+        const gobject_output = b.addWriteFiles();
+        _ = gobject_output.addCopyDirectory(bindings, b.fmt("ghostty-gobject-{s}", .{version}), .{});
+
+        b.installDirectory(.{
+            .source_dir = gobject_output.getDirectory(),
+            .install_dir = .{ .custom = "" },
+            .install_subdir = "",
+        });
+
+        const create_gobject_tar = b.addSystemCommand(&.{
+            "tar",
+            "--create",
+            "--dereference",
+            "--mtime=1970-01-01-T00:00:00+00:00",
+            "--mode=u=rwX,og=rX",
+            "--owner=root:0",
+            "--group=root:0",
+        });
+        create_gobject_tar.addPrefixedDirectoryArg("--directory=", gobject_output.getDirectory());
+        const gobject_tar = create_gobject_tar.addPrefixedOutputFileArg("--file=", b.fmt("ghostty-gobject-{s}.tar", .{version}));
+        create_gobject_tar.addArg(b.fmt("ghostty-gobject-{s}", .{version}));
+
+        const install_gobject_tarfile = b.addInstallFile(gobject_tar, b.fmt("ghostty-gobject-{s}.tar", .{version}));
+        b.getInstallStep().dependOn(&install_gobject_tarfile.step);
+
+        {
+            const name = b.fmt("ghostty-gobject-{s}.tar.gz", .{version});
+            const create_gobject_targz = b.addSystemCommand(&.{ "gzip", "-c" });
+            create_gobject_targz.addFileArg(gobject_tar);
+            const stdout = create_gobject_targz.captureStdOut();
+            const wf = b.addWriteFiles();
+            const gobject_targz = wf.addCopyFile(stdout, name);
+            const install_gobject_targz = b.addInstallFile(gobject_targz, name);
+            b.getInstallStep().dependOn(&install_gobject_targz.step);
+            try files_to_sign.append(b.allocator, .{ .name = name, .lp = gobject_targz });
+            try artifacts.append(b.allocator, gobject_targz);
+        }
+
+        {
+            const name = b.fmt("ghostty-gobject-{s}.tar.zstd", .{version});
+            const create_gobject_tarzstd = b.addSystemCommand(&.{ "zstd", "-c" });
+            create_gobject_tarzstd.addFileArg(gobject_tar);
+            const stdout = create_gobject_tarzstd.captureStdOut();
+            const wf = b.addWriteFiles();
+            const gobject_tarzstd = wf.addCopyFile(stdout, name);
+            const install_gobject_tarzstd = b.addInstallFile(gobject_tarzstd, name);
+            b.getInstallStep().dependOn(&install_gobject_tarzstd.step);
+            try files_to_sign.append(b.allocator, .{ .name = name, .lp = gobject_tarzstd });
+            try artifacts.append(b.allocator, gobject_tarzstd);
+        }
+    }
+
+    {
+        const gir_output = b.addWriteFiles();
+        _ = gir_output.addCopyDirectory(gir_files.getDirectory(), b.fmt("ghostty-gir-{s}", .{version}), .{});
+
+        b.installDirectory(.{
+            .source_dir = gir_output.getDirectory(),
+            .install_dir = .{ .custom = "" },
+            .install_subdir = "",
+        });
+
+        const create_gir_tar = b.addSystemCommand(&.{
+            "tar",
+            "--create",
+            "--dereference",
+            "--mtime=1970-01-01-T00:00:00+00:00",
+            "--mode=u=rwX,og=rX",
+            "--owner=root:0",
+            "--group=root:0",
+        });
+        create_gir_tar.addPrefixedDirectoryArg("--directory=", gir_output.getDirectory());
+        const gir_tar = create_gir_tar.addPrefixedOutputFileArg(
+            "--file=",
+            b.fmt("ghostty-gir-{s}.tar", .{version}),
+        );
+        create_gir_tar.addArg(b.fmt("ghostty-gir-{s}", .{version}));
+
+        {
+            const name = b.fmt("ghostty-gir-{s}.tar.gz", .{version});
+            const create_gir_targz = b.addSystemCommand(&.{ "gzip", "-c" });
+            create_gir_targz.addFileArg(gir_tar);
+            const stdout = create_gir_targz.captureStdOut();
+            const wf = b.addWriteFiles();
+            const gir_targz = wf.addCopyFile(stdout, name);
+            const install_gir_targz = b.addInstallFile(gir_targz, name);
+            b.getInstallStep().dependOn(&install_gir_targz.step);
+            try files_to_sign.append(b.allocator, .{ .name = name, .lp = gir_targz });
+            try artifacts.append(b.allocator, gir_targz);
+        }
+
+        {
+            const name = b.fmt("ghostty-gir-{s}.tar.zstd", .{version});
+            const create_gir_tarzstd = b.addSystemCommand(&.{ "zstd", "-c" });
+            create_gir_tarzstd.addFileArg(gir_tar);
+            const stdout = create_gir_tarzstd.captureStdOut();
+            const wf = b.addWriteFiles();
+            const gir_tarzstd = wf.addCopyFile(stdout, name);
+            const install_gir_tarzstd = b.addInstallFile(gir_tarzstd, name);
+            b.getInstallStep().dependOn(&install_gir_tarzstd.step);
+            try files_to_sign.append(b.allocator, .{ .name = name, .lp = gir_tarzstd });
+            try artifacts.append(b.allocator, gir_tarzstd);
+        }
+    }
+
+    minisign: {
+        const wf = b.addWriteFiles();
+        const minisign_key = wf.add("minisign.key", b.fmt("\n{s}\n", .{minisign_key_ orelse break :minisign}));
+        const minisign_password = wf.add("minisign.password", b.fmt("{s}", .{minisign_password_ orelse break :minisign}));
+
+        for (files_to_sign.items) |item| {
+            const name = b.fmt("{s}.minisig", .{item.name});
+            const minisign = b.addSystemCommand(&.{ "minisign", "-S" });
+            minisign.addArg("-m");
+            minisign.addFileArg(item.lp);
+            minisign.addArg("-x");
+            const minisig = minisign.addOutputFileArg(name);
+            minisign.addArg("-s");
+            minisign.addFileArg(minisign_key);
+            minisign.setStdIn(.{ .lazy_path = minisign_password });
+            const install_minisig = b.addInstallFile(minisig, name);
+            b.getInstallStep().dependOn(&install_minisig.step);
+            try artifacts.append(b.allocator, minisig);
+        }
+    }
+
+    const gh = b.addSystemCommand(
+        &.{
+            "gh",
+            "release",
+            "create",
+            version,
+            "--latest",
+            "--title",
+            version,
+            "--notes",
+            version,
+        },
+    );
+    for (artifacts.items) |lp| {
+        gh.addFileArg(lp);
+    }
+    const release_step = b.step("gh-release", "create a GitHub release");
+    release_step.dependOn(&gh.step);
 }
--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
               pkgs.gnutar
               pkgs.libxml2
               pkgs.libxslt
+              pkgs.minisign
               pkgs.nodePackages.prettier
               pkgs.pinact
               pkgs.pkg-config
@@ -61,66 +62,6 @@
             ++ gir_path;
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath gir_path;
           GIR_PATH = pkgs.lib.strings.makeSearchPathOutput "dev" "share/gir-1.0" gir_path;
-        };
-        packages.default = let
-          zig_hook = pkgs.zig_0_15.hook.overrideAttrs {
-            zig_default_flags = "--color off";
-          };
-        in
-          pkgs.stdenv.mkDerivation (finalAttrs: {
-            pname = "ghostty-gobject";
-            version = "0.5.0";
-            src = pkgs.lib.fileset.toSource {
-              root = ./.;
-              fileset = pkgs.lib.fileset.intersection (pkgs.lib.fileset.fromSource (pkgs.lib.sources.cleanSource ./.)) (
-                pkgs.lib.fileset.unions [
-                  ./build.zig
-                  ./build.zig.zon
-                  ./build.zig.zon.nix
-                  ./gir-fixes
-                ]
-              );
-            };
-            deps = pkgs.callPackage ./build.zig.zon.nix {
-              name = "${finalAttrs.pname}-cache-${finalAttrs.version}";
-            };
-            GIR_PATH = pkgs.lib.strings.makeSearchPathOutput "dev" "share/gir-1.0" gir_path;
-            nativeBuildInputs = [
-              zig_hook
-              pkgs.libxslt
-            ];
-            zigBuildFlags = [
-              "--system"
-              "${finalAttrs.deps}"
-            ];
-          });
-        apps.release = let
-          release = pkgs.writeTextFile {
-            name = "release";
-            destination = "/bin/release";
-            text = pkgs.lib.concatStringsSep "\n" [
-              ''
-                #!${pkgs.lib.getExe pkgs.nushell}
-
-                alias zig = ^${pkgs.lib.getExe pkgs.zig_0_15}
-                alias nix = ^${pkgs.lib.getExe pkgs.nix}
-                alias tar = ^${pkgs.lib.getExe pkgs.gnutar}
-                alias gh = ^${pkgs.lib.getExe pkgs.gh}
-                alias ln = ^${pkgs.uutils-coreutils}/bin/uutils-ln
-                alias readlink = ^${pkgs.uutils-coreutils}/bin/uutils-readlink
-                alias gzip = ^${pkgs.lib.getExe pkgs.gzip}
-                alias zstd = ^${pkgs.lib.getExe pkgs.zstd}
-                alias minisign = ^${pkgs.lib.getExe pkgs.minisign}
-
-                let gir_path = [ ${builtins.replaceStrings [":"] [" "] (pkgs.lib.strings.makeSearchPathOutput "dev" "share/gir-1.0" gir_path)} ]
-              ''
-              (builtins.readFile ./release.nu)
-            ];
-            executable = true;
-          };
-        in {
-          type = "app";
-          program = "${pkgs.lib.getExe release}";
         };
       }
     );
